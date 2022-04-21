68eef6032add1dd7b93ea04e4eb20b921330a465
MGonza20 <pag20634@uvg.edu.gt>
2022-04-20 21:41:57 -0600

    correcciones de eslint

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import Card from './Card'
-import './board.css'
+import React from 'react';
+import Card from './Card';
+import './board.css';
 
-export default function Board({cardsProp, onFlip, card1, card2, block}) { 
-  
+export default function Board({
+  cardsProp, onFlip, card1, card2, block,
+}) {
   return (
     <div className="board">
       {cardsProp.map((element, index) => (
-          <Card 
+        <Card
           key={index}
-          cardElement={element} 
-          onFlip={onFlip} 
+          cardElement={element}
+          onFlip={onFlip}
           block={block}
-          flipIt={element.paired || element === card1 || element === card2 }/>
+          flipIt={element.paired || element === card1 || element === card2}
+        />
       ))}
     </div>
-  )
+  );
 }
